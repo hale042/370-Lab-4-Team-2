891e7b66567b73ff26762fd7867f1c529ee7f5b1
Luke Hale <luahale@ucsc.edu>
2025-09-23 20:14:39 -0700

    update formatting

--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -14,6 +14,8 @@ public class Main {
     }
 
     public Main() {
+        final int IMAGE_WIDTH = 500 / 4;
+        final int IMAGE_HEIGHT = 726 / 4;
         File path = new File("Lab4/Poker cards/");
         File[] allFiles = path.listFiles();
         assert allFiles != null;
@@ -23,7 +25,11 @@ public class Main {
         Arrays.sort(allFiles);
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(4, 13));
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+
         JLabel[] label = new JLabel[allFiles.length];
             for (int i = 0; i < allFiles.length; i++) {
                 try {
@@ -31,25 +37,30 @@ public class Main {
                     allImages[i] = ImageIO.read(allFiles[i]);
                     label[i] = new JLabel();
 
-                    Image newImg = allImages[i].getScaledInstance(500 / 3, 726 / 3, Image.SCALE_SMOOTH); // scale it the smooth way
+                    Image newImg = allImages[i].getScaledInstance(IMAGE_WIDTH, IMAGE_HEIGHT, Image.SCALE_SMOOTH); // scale it the smooth way
                     ImageIcon scaledImage = new ImageIcon(newImg);
 
                     label[i].setIcon(scaledImage);
-                    frame.add(label[i]);
+                    c.gridx = i % 13;
+                    c.gridy = i / 13;
+                    frame.add(label[i], c);
 
                 } catch (IOException _) {
 
                 }
             }
+        c.gridx = 6;
+        c.gridy = 4;
 
-        JButton button1 = new JButton("button 1"); // button; setting its text
-        frame.getContentPane().add(button1); // Adds Button to content pane of frame
+        JButton button1 = new JButton("Shuffle"); // button; setting its text
+        button1.setPreferredSize(new Dimension(40, 40));
+        frame.add(button1, c); // Adds Button to content pane of frame
         button1.addActionListener(_ -> {
 
             Collections.shuffle(list);
 
                 for (int i = 0; i < allFiles.length; i++) {
-                    Image newImg = allImages[list.get(i)].getScaledInstance(500 / 3, 726 / 3, Image.SCALE_SMOOTH); // scale it the smooth way
+                    Image newImg = allImages[list.get(i)].getScaledInstance(IMAGE_WIDTH,IMAGE_HEIGHT, Image.SCALE_SMOOTH); // scale it the smooth way
                     ImageIcon scaledImage = new ImageIcon(newImg);
                     label[i].setIcon(scaledImage);
                 }
