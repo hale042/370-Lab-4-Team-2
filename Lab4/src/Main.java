653ff6266e54b18cf018063b14643410f5456046
gadget-the <96590338+gadget-the@users.noreply.github.com>
2025-09-23 20:00:50 -0700

    Update Main.java

--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -2,6 +2,8 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,34 +27,41 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(4, 13));
         JLabel[] label = new JLabel[allFiles.length];
-            for (int i = 0; i < allFiles.length; i++) {
-                try {
-                    list.add(i);
-                    allImages[i] = ImageIO.read(allFiles[i]);
-                    label[i] = new JLabel();
 
-                    Image newImg = allImages[i].getScaledInstance(500 / 3, 726 / 3, Image.SCALE_SMOOTH); // scale it the smooth way
-                    ImageIcon scaledImage = new ImageIcon(newImg);
+        int imageWidth = 500;
+        int imageHeight = 726;
+        int imageScaleValue = 6;
 
-                    label[i].setIcon(scaledImage);
-                    frame.add(label[i]);
+        for (int i = 0; i < allFiles.length; i++) {
+            try {
+                list.add(i);
+                allImages[i] = ImageIO.read(allFiles[i]);
+                label[i] = new JLabel();
 
-                } catch (IOException _) {
+                Image newImg = allImages[i].getScaledInstance(imageWidth / imageScaleValue, imageHeight / imageScaleValue, Image.SCALE_SMOOTH); // scale it the smooth way
+                ImageIcon scaledImage = new ImageIcon(newImg);
 
-                }
+                label[i].setIcon(scaledImage);
+                frame.add(label[i]);
+
+            } catch (IOException e) {
+
             }
+        }
 
         JButton button1 = new JButton("button 1"); // button; setting its text
         frame.getContentPane().add(button1); // Adds Button to content pane of frame
-        button1.addActionListener(_ -> {
-
-            Collections.shuffle(list);
+        button1.addActionListener(new ActionListener() {
+            @Override // overide might not be neccessary here
+            public void actionPerformed(ActionEvent E) {
+                Collections.shuffle(list);
 
                 for (int i = 0; i < allFiles.length; i++) {
-                    Image newImg = allImages[list.get(i)].getScaledInstance(500 / 3, 726 / 3, Image.SCALE_SMOOTH); // scale it the smooth way
+                    Image newImg = allImages[list.get(i)].getScaledInstance(imageWidth / imageScaleValue, imageHeight / imageScaleValue, Image.SCALE_SMOOTH); // scale it the smooth way
                     ImageIcon scaledImage = new ImageIcon(newImg);
                     label[i].setIcon(scaledImage);
                 }
+            }
         });
 
         frame.pack();
