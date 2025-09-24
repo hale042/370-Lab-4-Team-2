4044e3287298779a40f918536d693f9896aec28b
Luke Hale <luahale@ucsc.edu>
2025-09-23 20:22:24 -0700

    update formatting

--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -2,8 +2,6 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,6 +14,8 @@ public class Main {
     }
 
     public Main() {
+        final int IMAGE_WIDTH = 500 / 4;
+        final int IMAGE_HEIGHT = 726 / 4;
         File path = new File("Lab4/Poker cards/");
         File[] allFiles = path.listFiles();
         assert allFiles != null;
@@ -25,42 +25,44 @@ public class Main {
         Arrays.sort(allFiles);
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(4, 13));
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+
         JLabel[] label = new JLabel[allFiles.length];
-
-        int imageWidth = 500;
-        int imageHeight = 726;
-        int imageScaleValue = 6;
-
         for (int i = 0; i < allFiles.length; i++) {
             try {
                 list.add(i);
                 allImages[i] = ImageIO.read(allFiles[i]);
                 label[i] = new JLabel();
 
-                Image newImg = allImages[i].getScaledInstance(imageWidth / imageScaleValue, imageHeight / imageScaleValue, Image.SCALE_SMOOTH); // scale it the smooth way
+                Image newImg = allImages[i].getScaledInstance(IMAGE_WIDTH, IMAGE_HEIGHT, Image.SCALE_SMOOTH); // scale it the smooth way
                 ImageIcon scaledImage = new ImageIcon(newImg);
 
                 label[i].setIcon(scaledImage);
-                frame.add(label[i]);
+                c.gridx = i % 13;
+                c.gridy = i / 13;
+                frame.add(label[i], c);
 
-            } catch (IOException e) {
+            } catch (IOException _) {
 
             }
         }
+        c.gridx = 6;
+        c.gridy = 4;
 
-        JButton button1 = new JButton("button 1"); // button; setting its text
-        frame.getContentPane().add(button1); // Adds Button to content pane of frame
-        button1.addActionListener(new ActionListener() {
-            @Override // overide might not be neccessary here
-            public void actionPerformed(ActionEvent E) {
-                Collections.shuffle(list);
+        JButton button1 = new JButton("Shuffle"); // button; setting its text
+        button1.setPreferredSize(new Dimension(40, 40));
+        frame.add(button1, c); // Adds Button to content pane of frame
+        button1.addActionListener(_ -> {
 
-                for (int i = 0; i < allFiles.length; i++) {
-                    Image newImg = allImages[list.get(i)].getScaledInstance(imageWidth / imageScaleValue, imageHeight / imageScaleValue, Image.SCALE_SMOOTH); // scale it the smooth way
-                    ImageIcon scaledImage = new ImageIcon(newImg);
-                    label[i].setIcon(scaledImage);
-                }
+            Collections.shuffle(list);
+
+            for (int i = 0; i < allFiles.length; i++) {
+                Image newImg = allImages[list.get(i)].getScaledInstance(IMAGE_WIDTH,IMAGE_HEIGHT, Image.SCALE_SMOOTH); // scale it the smooth way
+                ImageIcon scaledImage = new ImageIcon(newImg);
+                label[i].setIcon(scaledImage);
             }
         });
 
